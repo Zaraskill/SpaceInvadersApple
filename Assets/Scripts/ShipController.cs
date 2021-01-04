d2713f34eacc08a5e1ef00ae0335303cdf1ab405
Corentin <corentin.lelong77@gmail.com>
2021-01-04 11:54:47 +0100

    Movement ship

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
+    private int direction;
+    private Rigidbody2D _rigidbody;
+
+    public float speed;
+    public GameObject projectile;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateInput();
+        UpdateMovement();
+    }
+
+    private void UpdateInput()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = -1;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = 1;
+        }
+        else
+        {
+            direction = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    private void UpdateMovement()
+    {
+        float movement = direction * speed;
+        transform.position += new Vector3(0.01f * movement, 0, 0);
+    }
+
+    private void Shoot()
+    {
+        //Instantiate(projectile);
+        //projectile.transform.position += Vector3.up;
     }
 }
