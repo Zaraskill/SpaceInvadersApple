4a4bd71f1f1e068421e1edadf4eec88dc35d705b
François <dessartsfran@gmail.com>
2021-01-05 10:33:28 +0100

    Invader move vertical

---
+++ Assets/Scripts/Invader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invader : MonoBehaviour
+{
+    public static List<Invader> list = new List<Invader>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        list.Add(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/InvaderManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvaderManager : MonoBehaviour
+{
+    [SerializeField] private float shootFrequency = 2f;
+
+    [SerializeField] private float moveSpeed = 0.1f;
+    [SerializeField] private float moveInterval = 2f;
+
+    private float elapsedTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if(elapsedTime >= moveInterval)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Vector3.down, moveSpeed);
+            elapsedTime = 0;
+        }  
+    }
+}
